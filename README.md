e17c4ad9d30672a45f8fd69ae7a0c53a8872cf3a
krypciak <krypek@tuta.io>
2022-08-15 16:02:44 +0200

    Initial commit

--- README.md
+++
@@ -1,7 +0,0 @@
-A Fabric Minecraft mod that allows you to measure the time in ms that the mcfunction takes to run.<br>
-Mod tested for 1.17 and 1.17.1.
-
-<a href="https://github.com/krypciak/MCFunction-Benchmark/blob/fd393a578bf1538e171a368b56c255f3b8a3d991/logo.png"><img src="https://github.com/krypciak/MCFunction-Benchmark/blob/fd393a578bf1538e171a368b56c255f3b8a3d991/logo.png" width="128" height="128"/></a>
-
-Usage:
-/mcfb **function _name**
--- src/main/java/me/krypek/mcfb/mcfb.java
+++ src/main/java/me/krypek/mcfb/mcfb.java
@@ -1,98 +1,74 @@
 package me.krypek.mcfb;
 
 import java.util.Collection;
-import java.util.Iterator;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.CommandSource;
 import net.minecraft.command.argument.CommandFunctionArgumentType;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.network.message.MessageType;
 import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.FunctionCommand;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.function.CommandFunction;
+import net.minecraft.server.function.CommandFunctionManager;
 import net.minecraft.text.Text;
 
 public class mcfb implements ModInitializer {
 
-	public void onInitialize() {
-		registerCommand();
-	}
+	@Override
+	public void onInitialize() { register(); }
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	void registerCommand() {
-		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-			dispatcher.register((LiteralArgumentBuilder) ((LiteralArgumentBuilder) CommandManager.literal("mcfb")
-					.requires((source) -> {
-						return source.hasPermissionLevel(2);
-					})).then(CommandManager.argument("name", CommandFunctionArgumentType.commandFunction())
-							.suggests(FunctionCommand.SUGGESTION_PROVIDER).executes((context) -> {
-								return execute(context);
-							})));
+	public static final SuggestionProvider<ServerCommandSource> SUGGESTION_PROVIDER = (context, builder) -> {
+		CommandFunctionManager commandFunctionManager = context.getSource().getServer().getCommandFunctionManager();
+		CommandSource.suggestIdentifiers(commandFunctionManager.getFunctionTags(), builder, "#");
+		return CommandSource.suggestIdentifiers(commandFunctionManager.getAllFunctions(), builder);
+	};
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void register() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register((LiteralArgumentBuilder<ServerCommandSource>) ((LiteralArgumentBuilder) CommandManager.literal("mcfb")
+					.requires(source -> source.hasPermissionLevel(2)))
+					// player selector
+					.then(CommandManager.argument("targets", EntityArgumentType.players())
+					// function
+					.then(CommandManager.argument("name", CommandFunctionArgumentType.commandFunction()).suggests(SUGGESTION_PROVIDER)
+					.executes(mcfb::execute))));
 		});
 	}
 
-	@SuppressWarnings("rawtypes")
-	private static int execute(CommandContext<ServerCommandSource> context) {
-
-		ServerCommandSource source = (ServerCommandSource) context.getSource();
-		Collection<CommandFunction> functions = null;
-		try {
-			functions = CommandFunctionArgumentType.getFunctions(context, "name");
-		} catch (CommandSyntaxException e) {
-			e.printStackTrace();
-		}
-
-		long time1 = System.currentTimeMillis();
+	private static int execute(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		ServerCommandSource source = context.getSource();
+		Collection<CommandFunction> functions = CommandFunctionArgumentType.getFunctions(context, "name");
+		String functionName = context.getInput().split(" ")[2];
 
 		int i = 0;
-
-		CommandFunction commandFunction;
-		for (Iterator var3 = functions.iterator(); var3.hasNext(); i += source.getServer().getCommandFunctionManager()
-				.execute(commandFunction, source.withSilent().withMaxLevel(2))) {
-			commandFunction = (CommandFunction) var3.next();
-		}
-
+		long time1 = System.currentTimeMillis();
+		for (CommandFunction commandFunction : functions)
+			i += source.getServer().getCommandFunctionManager().execute(commandFunction, source.withSilent().withMaxLevel(2));
 		long time2 = System.currentTimeMillis();
+		long totalTime = time2 - time1;
 
-		String functionName = context.getInput().split(" ")[1];
+		String msg = "§2§lFinished function: §o§7§l" + functionName + "§2§l! \n§6§lCommand Amout: §o§7§l" + i + "  \n§6§lTotal time: §o§7§l"
+				+ (time2 - time1) + " §6§lms";
 
-		// MCMPC stuff start
-		boolean doMCMPC_core_start_ips = true;
-		if (doMCMPC_core_start_ips) {
+		if(functionName.equals("core:start")) {
+			ScoreboardObjective scoreboardMCM = source.getWorld().getServer().getScoreboard().getObjective("mcm");
 
-			Text text;
-			if (functionName.equals("core:start") || functionName.equals("c:k")) {
-
-				ScoreboardObjective scoreboardMCM = source.getWorld().getServer().getScoreboard().getObjective("mcm");
-
-				int instructionAmount = source.getWorld().getServer().getScoreboard()
-						.getPlayerScore("commandAmount", scoreboardMCM).getScore();
-
-				long totalTime = (time2 - time1);
-
-				text = Text.of("§2§lFinished function: §o§7§l" + functionName + "§2§l! \n§6§lCommand Amout: §o§7§l" + i
-						+ "  \n§6§lTotal time: §o§7§l" + totalTime + " §6§lms\n§6§lInstruction Amount: §o§7§l"
-						+ instructionAmount + "\n§6§lIPS §8= §o§7§l" + instructionAmount + " §8/ §o§7§l"
-						+ (((double) totalTime) / 1000d) + " §8= §o§7§l"
-						+ (int) (instructionAmount / (((double) totalTime) / 1000d)));
-
-			} else {
-				text = Text.of("§2§lFinished function: §l§o§7§l" + functionName + "§2§l! \n§6§lCommand Amout: §l§o§7§l"
-						+ i + "  \n§6§lTotal time: §l§o§7§l" + (time2 - time1) + " §6§lms");
-			}
-			source.sendFeedback(text, false);
-
-		} // MCMPC stuff end
-		else {
-			Text text = Text.of("§2§lFinished function: §o§7§l" + functionName + "§2§l! \n§6§lCommand Amout: §o§7§l" + i
-					+ "  \n§6§lTotal time: §o§7§l" + (time2 - time1) + " §6§lms");
-			source.sendFeedback(text, false);
+			int instructionAmount = source.getWorld().getServer().getScoreboard().getPlayerScore("commandAmount", scoreboardMCM).getScore();
+			msg += " §6§lms\n§6§lInstruction Amount: §o§7§l" + instructionAmount + "\n§6§lIPS §8= §o§7§l" + instructionAmount + " §8/ §o§7§l"
+					+ totalTime / 1000d + " §8= §o§7§l" + (int) (instructionAmount / (totalTime / 1000d));
 		}
+
+		Text text = Text.of(msg);
+		EntityArgumentType.getPlayers(context, "targets").forEach(player -> player.sendMessage(text, MessageType.SYSTEM));
 
 		return i;
 	}
